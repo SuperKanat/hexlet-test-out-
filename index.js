e032aeb153a07cc13c19abbca14bca8b2f105dd3
kanat <vasilyev.yuriy@hexly.ru>
2023-05-29 22:57:22 +0300

    lol

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 export default function solution(content){
   // BEGIN
+  //1
+  const data = content.split('\n').slice(1, -1)
+  const superData = data.map((lol) => lol)
+  console.log(`Count: ${superData.length}`)
+
+  //2
+
+  //3
+
+  //4
+
+  //5
 
   // END
 }
